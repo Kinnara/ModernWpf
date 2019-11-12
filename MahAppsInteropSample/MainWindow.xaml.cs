bdf13900c51f9b339fb1e208677d3b276c880da3
Yimeng Wu <me@kinnara.cn>
2019-11-12 13:50:48 +0800

    Add MahApps.Metro interop sample

---
+++ MahAppsInteropSample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ModernWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MahAppsInteropSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InvertTheme(object sender, RoutedEventArgs e)
+        {
+            var tm = ThemeManager.Current;
+            if (tm.ActualApplicationTheme == ApplicationTheme.Dark)
+            {
+                tm.ApplicationTheme = ApplicationTheme.Light;
+            }
+            else
+            {
+                tm.ApplicationTheme = ApplicationTheme.Dark;
+            }
+        }
+    }
+}
