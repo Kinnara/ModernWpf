61a0f0d1096db10c49b94104d06996fb1b083c81
Yimeng Wu <me@kinnara.cn>
2019-11-13 00:44:47 +0800

    Improve sample app

--- ModernWpf.SampleApp/ControlPages/WindowPage.xaml.cs
+++ ModernWpf.SampleApp/ControlPages/WindowPage.xaml.cs
@@ -17,5 +17,15 @@ namespace ModernWpf.SampleApp.ControlPages
             w.ClearValue(TitleBar.BackgroundProperty);
             w.ClearValue(TitleBar.ForegroundProperty);
         }
+
+        private void ResizeWindow(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is MenuItem menuItem)
+            {
+                var size = Size.Parse((string)menuItem.Tag);
+                Application.Current.MainWindow.Width = size.Width;
+                Application.Current.MainWindow.Height = size.Height;
+            }
+        }
     }
 }
