b3defe89bbfa1d915441123310c0c24ae3a8d1d3
Yimeng Wu <me@kinnara.cn>
2019-11-14 12:12:33 +0800

    Remove duplicate TypedEventHandler type

--- ModernWpf.Controls/Common/TypedEventHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-namespace ModernWpf.Controls
-{
-    public delegate void TypedEventHandler<TSender, TResult>(TSender sender, TResult args);
-}
