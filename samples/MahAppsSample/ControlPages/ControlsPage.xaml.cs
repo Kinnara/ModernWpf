589f7fda1bbafb3effc62812737e020256d7b1a1
Yimeng Wu <me@kinnara.cn>
2019-11-19 00:41:36 +0800

    Improve samples

---
+++ samples/MahAppsSample/ControlPages/ControlsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MahAppsSample.ControlPages
+{
+    public partial class ControlsPage : Page
+    {
+        public ControlsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
