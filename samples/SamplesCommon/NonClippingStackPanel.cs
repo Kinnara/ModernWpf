3e8389b107ec264b4084de2173697cb2e48aba80
Yimeng Wu <me@kinnara.cn>
2019-11-19 02:31:53 +0800

    Improve samples

---
+++ samples/SamplesCommon/NonClippingStackPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SamplesCommon
+{
+    public class NonClippingStackPanel : StackPanel
+    {
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            return null;
+        }
+    }
+}
