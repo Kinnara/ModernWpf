289113bfd9ca7db7dcc75eaff4017d5a36a3e0a8
Yimeng Wu <me@kinnara.cn>
2019-12-12 20:21:45 +0800

    Fix issue where scroll bar thumb color didn’t always change with theme

---
+++ ModernWpf/Controls/Primitives/ScrollBarThumbHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public static class ScrollBarThumbHelper
+    {
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsExpanded",
+                typeof(bool),
+                typeof(ScrollBarThumbHelper),
+                new PropertyMetadata(false));
+
+        public static bool GetIsExpanded(Thumb thumb)
+        {
+            return (bool)thumb.GetValue(IsExpandedProperty);
+        }
+
+        public static void SetIsExpanded(Thumb thumb, bool value)
+        {
+            thumb.SetValue(IsExpandedProperty, value);
+        }
+    }
+}
