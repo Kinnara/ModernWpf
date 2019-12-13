5410cb87647ea3a971c2034be69c89a13db474c2
Yimeng Wu <me@kinnara.cn>
2019-12-14 01:41:51 +0800

    Add PowerShell sample

---
+++ samples/PowerShellSample/run.ps1
@@ -0,0 +1,10 @@
+﻿Set-Location $PSScriptRoot
+[IO.Directory]::SetCurrentDirectory($PSScriptRoot)
+
+Add-Type -AssemblyName PresentationFramework
+[Reflection.Assembly]::LoadFrom('bin\Debug\net45\System.ValueTuple.dll') | Out-Null
+[Reflection.Assembly]::LoadFrom('bin\Debug\net45\ModernWpf.dll') | Out-Null
+
+$xaml = Get-Content "MainWindow.xaml" -Raw
+$window = [Windows.Markup.XamlReader]::Parse($xaml)
+$window.ShowDialog()
