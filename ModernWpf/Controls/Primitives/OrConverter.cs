413e863c5f01aaedb98bc238dbea85d7ed217e23
Yimeng Wu <me@kinnara.cn>
2020-01-06 20:12:38 +0800

    Keep text controls in focused state while their context menu is open

---
+++ ModernWpf/Controls/Primitives/OrConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public class OrConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is bool b && b)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
