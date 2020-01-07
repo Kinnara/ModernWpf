ca7c8a28ea1cb90043d05509e622143c7d51ed29
Yimeng Wu <me@kinnara.cn>
2020-01-07 16:15:35 +0800

    Make sample apps respect system Transparency effects setting

---
+++ samples/SamplesCommon/UISettingsResources.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+using Windows.Foundation.Metadata;
+using Windows.UI.ViewManagement;
+
+namespace SamplesCommon
+{
+    public class UISettingsResources : ResourceDictionary
+    {
+        private object _uiSettings;
+
+        public UISettingsResources()
+        {
+            if (!DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                if (Environment.OSVersion.Version.Major >= 10)
+                {
+                    Initialize();
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Initialize()
+        {
+            if (_uiSettings != null)
+            {
+                return;
+            }
+
+            var uiSettings = new UISettings();
+
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 4))
+            {
+                InitializeForContract4(uiSettings);
+            }
+
+            _uiSettings = uiSettings;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void InitializeForContract4(UISettings settings)
+        {
+            settings.AdvancedEffectsEnabledChanged += (sender, args) =>
+            {
+                Application.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    ApplyAdvancedEffectsEnabled(sender.AdvancedEffectsEnabled);
+                });
+            };
+            ApplyAdvancedEffectsEnabled(settings.AdvancedEffectsEnabled);
+        }
+
+        private void ApplyAdvancedEffectsEnabled(bool value)
+        {
+            var key = SystemParameters.DropShadowKey;
+            if (value)
+            {
+                Remove(key);
+            }
+            else
+            {
+                this[key] = false;
+            }
+        }
+    }
+}
