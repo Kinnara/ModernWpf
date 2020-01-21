a64e593ec20da8a9050b8b9fc054b55336d26817
Yimeng Wu <me@kinnara.cn>
2020-01-21 15:17:10 +0800

    Fix #25

---
+++ ModernWpf.SampleApp/Common/CornerRadiusToDoubleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ModernWpf.SampleApp.Common
+{
+    public class CornerRadiusToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cr = (CornerRadius)value;
+            if (cr.TopLeft == cr.TopRight &&
+                cr.TopLeft == cr.BottomRight &&
+                cr.TopLeft == cr.BottomRight)
+            {
+                return cr.TopLeft;
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new CornerRadius((double)value);
+        }
+    }
+}
