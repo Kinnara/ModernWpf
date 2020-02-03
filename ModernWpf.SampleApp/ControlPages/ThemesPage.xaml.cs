0e378e42e6df2f1081d144f8330a3d7aec0ee332
Yimeng Wu <me@kinnara.cn>
2020-02-03 14:49:57 +0800

    Add corner radius sliders to sample app

--- ModernWpf.SampleApp/ControlPages/ThemesPage.xaml.cs
+++ ModernWpf.SampleApp/ControlPages/ThemesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ModernWpf.SampleApp.ControlPages
@@ -8,6 +9,14 @@ namespace ModernWpf.SampleApp.ControlPages
         public ThemesPage()
         {
             InitializeComponent();
+
+            var controlCornerRadius = (CornerRadius)Application.Current.FindResource("ControlCornerRadius");
+            ControlCornerRadiusSlider.Value = controlCornerRadius.TopLeft;
+            ControlCornerRadiusSlider.ValueChanged += ControlCornerRadiusSlider_ValueChanged;
+
+            var overlayCornerRadius = (CornerRadius)Application.Current.FindResource("OverlayCornerRadius");
+            OverlayCornerRadiusSlider.Value = overlayCornerRadius.TopLeft;
+            OverlayCornerRadiusSlider.ValueChanged += OverlayCornerRadiusSlider_ValueChanged;
 
             //WindowThemeSelector.SetBinding(
             //    RadioButtons.SelectedItemProperty,
@@ -28,9 +37,22 @@ namespace ModernWpf.SampleApp.ControlPages
             new Window { Title = "New Window" }.Show();
         }
 
-        private Window GetWindow()
+        private void ControlCornerRadiusSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            return Application.Current.MainWindow;
+            SetThemeResource("ControlCornerRadius", new CornerRadius(e.NewValue));
+        }
+
+        private void OverlayCornerRadiusSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SetThemeResource("OverlayCornerRadius", new CornerRadius(e.NewValue));
+        }
+
+        private void SetThemeResource(object key, object value)
+        {
+            var tr = Application.Current.Resources.MergedDictionaries.OfType<ThemeResources>().First();
+            tr.ThemeDictionaries["Light"][key] = value;
+            tr.ThemeDictionaries["Dark"][key] = value;
+            tr.ThemeDictionaries["HighContrast"][key] = value;
         }
     }
 }
