26d196d1e20a8df6c77457ae180c015d46ac9131
Yimeng Wu <me@kinnara.cn>
2020-02-04 13:20:28 +0800

    Add common styles test page

---
+++ ModernWpf/Controls/Primitives/ColumnDefinitionHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public static class ColumnDefinitionHelper
+    {
+        #region PixelWidth
+
+        public static readonly DependencyProperty PixelWidthProperty =
+            DependencyProperty.RegisterAttached(
+                "PixelWidth",
+                typeof(double),
+                typeof(ColumnDefinitionHelper),
+                new PropertyMetadata(double.NaN, OnPixelWidthChanged));
+
+        public static double GetPixelWidth(ColumnDefinition columnDefinition)
+        {
+            return (double)columnDefinition.GetValue(PixelWidthProperty);
+        }
+
+        public static void SetPixelWidth(ColumnDefinition columnDefinition, double value)
+        {
+            columnDefinition.SetValue(PixelWidthProperty, value);
+        }
+
+        private static void OnPixelWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var columnDefinition = (ColumnDefinition)d;
+            var pixels = (double)e.NewValue;
+            if (double.IsNaN(pixels) || double.IsInfinity(pixels))
+            {
+                columnDefinition.ClearValue(ColumnDefinition.WidthProperty);
+            }
+            else
+            {
+                columnDefinition.Width = new GridLength(pixels);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ModernWpf/Controls/Primitives/RowDefinitionHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public static class RowDefinitionHelper
+    {
+        #region PixelHeight
+
+        public static readonly DependencyProperty PixelHeightProperty =
+            DependencyProperty.RegisterAttached(
+                "PixelHeight",
+                typeof(double),
+                typeof(RowDefinitionHelper),
+                new PropertyMetadata(double.NaN, OnPixelHeightChanged));
+
+        public static double GetPixelHeight(RowDefinition rowDefinition)
+        {
+            return (double)rowDefinition.GetValue(PixelHeightProperty);
+        }
+
+        public static void SetPixelHeight(RowDefinition rowDefinition, double value)
+        {
+            rowDefinition.SetValue(PixelHeightProperty, value);
+        }
+
+        private static void OnPixelHeightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var rowDefinition = (RowDefinition)d;
+            var pixels = (double)e.NewValue;
+            if (double.IsNaN(pixels) || double.IsInfinity(pixels))
+            {
+                rowDefinition.ClearValue(RowDefinition.HeightProperty);
+            }
+            else
+            {
+                rowDefinition.Height = new GridLength(pixels);
+            }
+        }
+
+        #endregion
+    }
+}
