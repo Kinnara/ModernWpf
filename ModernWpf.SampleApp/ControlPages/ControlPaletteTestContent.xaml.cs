f5bae9754c365ff11b0baf816119a3d0875a9cb5
Yimeng Wu <me@kinnara.cn>
2020-04-02 16:26:28 +0800

    Improve test content

--- ModernWpf.SampleApp/ControlPages/ControlPaletteTestContent.xaml.cs
+++ ModernWpf.SampleApp/ControlPages/ControlPaletteTestContent.xaml.cs
@@ -1,25 +1,9 @@
-﻿using ModernWpf;
-using ModernWpf.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ModernWpf.SampleApp.ControlPages
 {
-    /// <summary>
-    /// Interaction logic for ControlPaletteTestContent.xaml
-    /// </summary>
     public partial class ControlPaletteTestContent : UserControl
     {
         public ControlPaletteTestContent()
@@ -42,5 +26,10 @@ namespace ModernWpf.SampleApp.ControlPages
             new PropertyMetadata(string.Empty));
 
         #endregion
+
+        private void RadioButton_Initialized(object sender, EventArgs e)
+        {
+            ((RadioButton)sender).GroupName = Guid.NewGuid().ToString();
+        }
     }
 }
