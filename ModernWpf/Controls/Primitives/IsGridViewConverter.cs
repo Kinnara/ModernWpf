55f8b5104e5d45811e7f0f84a1694f8ac23fde3e
Yimeng Wu <me@kinnara.cn>
2020-06-15 00:34:18 +0800

    Remove the need to set an explicit style for a ListView that uses GridView (#114)

---
+++ ModernWpf/Controls/Primitives/IsGridViewConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public class IsGridViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is System.Windows.Controls.GridView;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
