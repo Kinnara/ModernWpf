09c21ce03f31ce541660a404108b33d08521a0ea
Shankar <shankarbus04082001@hotmail.com>
2020-11-05 12:21:25 +0530

    Added headers with copyright & license notices to avoid potential licensing issues

--- ModernWpf/IconSource/IconSource.cs
+++ ModernWpf/IconSource/IconSource.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Windows;
 using System.Windows.Media;
+
 namespace ModernWpf.Controls
 {
     /// <summary>
