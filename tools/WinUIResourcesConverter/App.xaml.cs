00c01038520c44e04da84d353ca4b675ad950886
Shankar <shankarbus04082001@hotmail.com>
2020-11-17 22:49:38 +0530

    Added automation tool to convert RESW files to RESX files

---
+++ tools/WinUIResourcesConverter/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WinUIResourcesConverter
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ tools/WinUIResourcesConverter/ResourcesFile.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WinUIResourcesConverter
+{
+    public class ResourcesFile : INotifyPropertyChanged
+    {
+        internal const string DefaultResourcesFileName = "Resources";
+
+        public ResourcesFile(string langName)
+        {
+            LanguageName = langName;
+        }
+
+        public string LanguageName { get; }
+
+        private bool hasConverted;
+
+        public bool HasConverted
+        {
+            get => hasConverted;
+            set
+            {
+                if (value != hasConverted)
+                {
+                    hasConverted = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
