ed013453e1bb06505d335ce81e5a6b199537f309
wherewhere <qq2518766683@outlook.com>
2022-03-25 22:13:29 +0800

    Expander Compeleted Add ButtonPage

---
+++ ModernWpf.SampleApp/Common/BoolNegationConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ModernWpf.SampleApp.Common
+{
+    /// <summary>
+    /// Value converter that applies NOT operator to a <see cref="bool"/> value.
+    /// </summary>
+    public class BoolNegationConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a boolean value to its negation.
+        /// </summary>
+        /// <param name="value">The <see cref="bool"/> value to negate.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">Optional parameter. Not used.</param>
+        /// <param name="language">The language of the conversion. Not used</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+
+        /// <summary>
+        /// Convert back a boolean value to its negation.
+        /// </summary>
+        /// <param name="value">The <see cref="bool"/> value to negate.</param>
+        /// <param name="targetType">The type of the target property, as a type reference.</param>
+        /// <param name="parameter">Optional parameter. Not used.</param>
+        /// <param name="language">The language of the conversion. Not used</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+    }
+}
