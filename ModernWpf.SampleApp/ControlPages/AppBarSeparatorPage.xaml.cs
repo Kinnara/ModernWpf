f9e8d9784dda2609dcfe891e5779cd543ac0b122
wherewhere <qq2518766683@outlook.com>
2022-03-26 16:52:39 +0800

    Update AppBarSeparatorPage Update AppBarToggleButtonPage

--- ModernWpf.SampleApp/ControlPages/AppBarSeparatorPage.xaml.cs
+++ ModernWpf.SampleApp/ControlPages/AppBarSeparatorPage.xaml.cs
@@ -7,8 +7,10 @@ namespace ModernWpf.SampleApp.ControlPages
 {
     public partial class AppBarSeparatorPage
     {
-        AppBarToggleButton compactButton = null;
-        AppBarSeparator separator = null;
+        private CommandBar Control1;
+
+        private AppBarToggleButton compactButton = null;
+        private AppBarSeparator separator = null;
 
         public AppBarSeparatorPage()
         {
@@ -19,10 +21,10 @@ namespace ModernWpf.SampleApp.ControlPages
 
         private void AppBarSeparatorPage_Unloaded(object sender, RoutedEventArgs e)
         {
-            //CommandBar appBar = NavigationRootPage.Current.TopCommandBar;
-            //compactButton.Click -= CompactButton_Click;
-            //appBar.PrimaryCommands.Remove(compactButton);
-            //appBar.PrimaryCommands.Remove(separator);
+            CommandBar appBar = NavigationRootPage.Current.PageHeader.TopCommandBar;
+            compactButton.Click -= CompactButton_Click;
+            appBar.PrimaryCommands.Remove(compactButton);
+            appBar.PrimaryCommands.Remove(separator);
         }
 
         void AppBarButtonPage_Loaded(object sender, RoutedEventArgs e)
@@ -30,29 +32,42 @@ namespace ModernWpf.SampleApp.ControlPages
             // Add compact button to the command bar. It provides functionality specific
             // to this page, and is removed when leaving the page.
 
-            //CommandBar appBar = NavigationRootPage.Current.TopCommandBar;
-            //separator = new AppBarSeparator();
-            //appBar.PrimaryCommands.Insert(0, separator);
+            CommandBar appBar = NavigationRootPage.Current.PageHeader.TopCommandBar;
+            separator = new AppBarSeparator();
+            appBar.PrimaryCommands.Insert(0, separator);
 
-            //compactButton = new AppBarToggleButton();
-            //compactButton.Icon = new SymbolIcon(Symbol.FontSize);
-            //compactButton.Label = "IsCompact";
-            //compactButton.Click += CompactButton_Click;
-            //appBar.PrimaryCommands.Insert(0, compactButton);
+            compactButton = new AppBarToggleButton
+            {
+                Icon = new SymbolIcon(Symbol.FontSize),
+                Label = "IsCompact"
+            };
+            compactButton.Click += CompactButton_Click;
+            appBar.PrimaryCommands.Insert(0, compactButton);
         }
 
         private void CompactButton_Click(object sender, RoutedEventArgs e)
         {
-            // NOTE: If you use AppBarButton and AppBarSeparator controls in a CommandBar,
-            // the CommandBar sets the IsCompact property automatically. You only set it
-            // yourself if the control in not in a CommandBar.
-
-            ToggleButton toggle = sender as ToggleButton;
-            if (toggle != null && toggle.IsChecked != null)
+            if ((sender as AppBarToggleButton).IsChecked == true)
             {
-                foreach (ICommandBarElement element in Control1.Children)
+                Control1.DefaultLabelPosition = CommandBarDefaultLabelPosition.Collapsed;
+            }
+            else
+            {
+                Control1.DefaultLabelPosition = CommandBarDefaultLabelPosition.Bottom;
+            }
+        }
+
+        private void CommandBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is CommandBar b)
+            {
+                string name = b.Tag.ToString();
+
+                switch (name)
                 {
-                    element.IsCompact = (bool)toggle.IsChecked;
+                    case "Control1":
+                        Control1 = b;
+                        break;
                 }
             }
         }
