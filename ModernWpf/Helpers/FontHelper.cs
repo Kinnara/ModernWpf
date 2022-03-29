7cf1a5a30812054f512c469e31baccea2ec6aff6
wherewhere <qq2518766683@outlook.com>
2022-03-29 20:32:20 +0800

    Use Fluent System Icons as icon.

---
+++ ModernWpf/Helpers/FontHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ModernWpf.Helpers
+{
+    internal static class FontHelper
+    {
+        public static string SymbolThemeFontFamilyKey => GetSymbolThemeFontFamily();
+
+        private static string GetSymbolThemeFontFamily()
+        {
+            if (CheckSysFontExisting("Segoe Fluent Icons"))
+            {
+                return "SegoeFluentIcons";
+            }
+            else if (CheckSysFontExisting("Segoe MDL2 Assets"))
+            {
+                return "SegoeMDL2Assets";
+            }
+            else if (CheckSysFontExisting("Segoe UI Symbol"))
+            {
+                return "SegoeUISymbol";
+            }
+            else
+            {
+                return "FluentSystemIcons";
+            }
+        }
+
+        public static bool CheckSysFontExisting(string fontName = "文鼎細黑")
+        {
+            FontFamily font;
+            try
+            {
+                font = new FontFamily(fontName);
+                if (!font.FamilyNames.Any())
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
