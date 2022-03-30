403ba7d72dffc10bfd515f8aa9664dc9848e011b
wherewhere <qq2518766683@outlook.com>
2022-03-30 20:47:03 +0800

    Add GridPage

---
+++ ModernWpf.SampleApp/ControlPages/GridPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernWpf.SampleApp.ControlPages
+{
+    /// <summary>
+    /// GridPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class GridPage : Page
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ControlExampleSubstitution Substitution1 = new ControlExampleSubstitution
+            {
+                Key = "Column",
+            };
+            BindingOperations.SetBinding(Substitution1, ControlExampleSubstitution.ValueProperty, new Binding
+            {
+                Source = ColumnSlider,
+                Path = new PropertyPath("Value"),
+            });
+            ControlExampleSubstitution Substitution2 = new ControlExampleSubstitution
+            {
+                Key = "Row",
+            };
+            BindingOperations.SetBinding(Substitution2, ControlExampleSubstitution.ValueProperty, new Binding
+            {
+                Source = RowSlider,
+                Path = new PropertyPath("Value"),
+            });
+            Example1.Substitutions = new List<ControlExampleSubstitution> { Substitution1, Substitution2 };
+        }
+    }
+}
