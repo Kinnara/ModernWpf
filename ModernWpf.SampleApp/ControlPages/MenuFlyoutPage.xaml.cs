6215d467a1d207fdc352871506ec175e5b22b89f
wherewhere <qq2518766683@outlook.com>
2022-03-31 18:20:23 +0800

    Update MenuFlyoutPage

--- ModernWpf.SampleApp/ControlPages/MenuFlyoutPage.xaml.cs
+++ ModernWpf.SampleApp/ControlPages/MenuFlyoutPage.xaml.cs
@@ -1,19 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using ModernWpf.Controls;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Page = ModernWpf.Controls.Page;
 
 namespace ModernWpf.SampleApp.ControlPages
 {
-    public partial class MenuFlyoutPage
+    public partial class MenuFlyoutPage : Page
     {
         public MenuFlyoutPage()
         {
             InitializeComponent();
         }
 
-        private void MenuFlyoutItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
         {
-            MenuItem selectedItem = sender as MenuItem;
-
-            if (selectedItem != null)
+            if (sender is MenuItem selectedItem)
             {
                 string sortOption = selectedItem.Tag.ToString();
                 switch (sortOption)
@@ -30,6 +33,30 @@ namespace ModernWpf.SampleApp.ControlPages
                 }
                 Control1Output.Text = "Sort by: " + sortOption;
             }
+        }
+
+        private void Example5_Loaded(object sender, RoutedEventArgs e)
+        {
+            ControlExampleSubstitution Substitution1 = new ControlExampleSubstitution
+            {
+                Key = "RepeatToggle",
+            };
+            BindingOperations.SetBinding(Substitution1, ControlExampleSubstitution.ValueProperty, new Binding
+            {
+                Source = RepeatToggleMenuFlyoutItem,
+                Path = new PropertyPath("IsChecked"),
+            });
+            ControlExampleSubstitution Substitution2 = new ControlExampleSubstitution
+            {
+                Key = "ShuffleToggle",
+            };
+            BindingOperations.SetBinding(Substitution2, ControlExampleSubstitution.ValueProperty, new Binding
+            {
+                Source = ShuffleToggleMenuFlyoutItem,
+                Path = new PropertyPath("IsChecked"),
+            });
+            List<ControlExampleSubstitution> Substitutions = new List<ControlExampleSubstitution> { Substitution1, Substitution2 };
+            Example2.Substitutions = Substitutions;
         }
     }
 }
