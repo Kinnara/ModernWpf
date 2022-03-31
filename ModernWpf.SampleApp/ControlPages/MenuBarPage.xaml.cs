617466b0784c530beee1ed0be1a03e3596b4aab9
wherewhere <qq2518766683@outlook.com>
2022-03-31 18:39:39 +0800

    Add MenuBarPage

---
+++ ModernWpf.SampleApp/ControlPages/MenuBarPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Page = ModernWpf.Controls.Page;
+
+namespace ModernWpf.SampleApp.ControlPages
+{
+    /// <summary>
+    /// MenuBarPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class MenuBarPage : Page
+    {
+        public MenuBarPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnElementClicked(object sender, RoutedEventArgs e)
+        {
+            var selectedFlyoutItem = sender as MenuItem;
+            string exampleNumber = selectedFlyoutItem.Name.Substring(0, 1);
+            if (exampleNumber == "o")
+            {
+                SelectedOptionText.Text = "You clicked: " + (sender as MenuItem).Header;
+            }
+            else if (exampleNumber == "t")
+            {
+                SelectedOptionText1.Text = "You clicked: " + (sender as MenuItem).Header;
+            }
+            else if (exampleNumber == "z")
+            {
+                SelectedOptionText2.Text = "You clicked: " + (sender as MenuItem).Header;
+            }
+        }
+    }
+}
