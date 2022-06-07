92c6d9ebbdc9812b09120b4fe34e54b96d68552d
wherewhere <qq2518766683@outlook.com>
2022-06-07 19:39:20 +0800

    ProgressRing Completed

--- ModernWpf.Controls/ProgressRing/ProgressRing.cs
+++ ModernWpf.Controls/ProgressRing/ProgressRing.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
 using ModernWpf.Automation.Peers;
 using ModernWpf.Controls.Primitives;
 
@@ -39,11 +41,72 @@ namespace ModernWpf.Controls
                 nameof(IsActive),
                 typeof(bool),
                 typeof(ProgressRing),
-                new FrameworkPropertyMetadata(OnIsActivePropertyChanged));
+                new PropertyMetadata(true, OnIsActivePropertyChanged));
 
         private static void OnIsActivePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
         {
             ((ProgressRing)sender).OnIsActivePropertyChanged(args);
+        }
+
+        #endregion
+
+        #region IsIndeterminate
+
+        public bool IsIndeterminate
+        {
+            get => (bool)GetValue(IsIndeterminateProperty);
+            set => SetValue(IsIndeterminateProperty, value);
+        }
+
+        public static readonly DependencyProperty IsIndeterminateProperty =
+            DependencyProperty.Register(
+                nameof(IsIndeterminate),
+                typeof(bool),
+                typeof(ProgressRing),
+                new PropertyMetadata(true));
+
+        #endregion
+
+        #region Maximum
+
+        public double Maximum
+        {
+            get => (double)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register(
+                nameof(Maximum),
+                typeof(double),
+                typeof(ProgressRing),
+                new FrameworkPropertyMetadata(OnMaximumPropertyChanged));
+
+        private static void OnMaximumPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((ProgressRing)sender).OnRangeBasePropertyChanged(args);
+        }
+
+        #endregion
+
+        #region Minimum
+
+        public double Minimum
+        {
+            get => (double)GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register(
+                nameof(Minimum),
+                typeof(double),
+                typeof(ProgressRing),
+                new FrameworkPropertyMetadata(OnMinimumPropertyChanged));
+
+        private static void OnMinimumPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((ProgressRing)sender).OnRangeBasePropertyChanged(args);
         }
 
         #endregion
@@ -67,6 +130,50 @@ namespace ModernWpf.Controls
 
         #endregion
 
+        #region Value
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(double),
+                typeof(ProgressRing),
+                new FrameworkPropertyMetadata(OnValuePropertyChanged));
+
+        private static void OnValuePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((ProgressRing)sender).OnRangeBasePropertyChanged(args);
+        }
+        
+        #endregion
+
+        #region StrokeThickness
+
+        public double StrokeThickness
+        {
+            get => (double)GetValue(StrokeThicknessProperty);
+            set => SetValue(StrokeThicknessProperty, value);
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register(
+                nameof(StrokeThickness),
+                typeof(double),
+                typeof(ProgressRing),
+                new FrameworkPropertyMetadata(OnStrokeThicknessPropertyChanged));
+
+        private static void OnStrokeThicknessPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((ProgressRing)sender).OnStrokeThicknessPropertyChanged(args);
+        }
+
+        #endregion
+
         protected override AutomationPeer OnCreateAutomationPeer()
         {
             return new ProgressRingAutomationPeer(this);
@@ -74,20 +181,136 @@ namespace ModernWpf.Controls
 
         public override void OnApplyTemplate()
         {
-            base.OnApplyTemplate();
+            ApplyTemplateSettings();
 
-            ChangeVisualState();
+            base.OnApplyTemplate();
+        }
+
+        void ApplyTemplateSettings()
+        {
+            // TemplateSetting properties from WUXC for backwards compatibility.
+            var templateSettings = TemplateSettings;
+
+            ArcSegment OutlineArcPart = new ArcSegment
+            {
+                IsLargeArc = true,
+                SweepDirection = SweepDirection.Clockwise
+            };
+
+            BindingOperations.SetBinding(OutlineArcPart, ArcSegment.PointProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.OutlineArcPoint))
+            });
+
+            BindingOperations.SetBinding(OutlineArcPart, ArcSegment.SizeProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.OutlineArcSize))
+            });
+
+            PathFigure OutlineFigurePart = new PathFigure
+            {
+                Segments = new PathSegmentCollection
+                {
+                    OutlineArcPart
+                }
+            };
+
+            BindingOperations.SetBinding(OutlineFigurePart, PathFigure.StartPointProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.OutlineFigureStartPoint))
+            });
+
+            PathGeometry OutlinePath = new PathGeometry
+            {
+                Figures = new PathFigureCollection
+                {
+                    OutlineFigurePart
+                }
+            };
+
+            templateSettings.OutlinePath = OutlinePath;
+
+            ArcSegment RingArcPart = new ArcSegment
+            {
+                IsLargeArc = true,
+                SweepDirection = SweepDirection.Clockwise
+            };
+
+            BindingOperations.SetBinding(RingArcPart, ArcSegment.IsLargeArcProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.RingArcIsLargeArc))
+            });
+
+            BindingOperations.SetBinding(RingArcPart, ArcSegment.PointProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.RingArcPoint))
+            });
+
+            BindingOperations.SetBinding(RingArcPart, ArcSegment.SizeProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.RingArcSize))
+            });
+
+            PathFigure RingFigurePart = new PathFigure
+            {
+                Segments = new PathSegmentCollection
+                {
+                    RingArcPart
+                }
+            };
+
+            BindingOperations.SetBinding(RingFigurePart, PathFigure.StartPointProperty, new Binding
+            {
+                Source = templateSettings,
+                Path = new PropertyPath(nameof(TemplateSettings.RingFigureStartPoint))
+            });
+
+            PathGeometry RingPath = new PathGeometry
+            {
+                Figures = new PathFigureCollection
+                {
+                    RingFigurePart
+                }
+            };
+
+            templateSettings.RingPath = RingPath;
         }
 
         void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            ApplyTemplateSettings();
+            UpdateSize();
             ChangeVisualState();
+            UpdateRing();
+        }
+
+        void OnRangeBasePropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            UpdateSegment();
         }
 
         void OnIsActivePropertyChanged(DependencyPropertyChangedEventArgs args)
         {
             ChangeVisualState();
+        }
+
+        void OnStrokeThicknessPropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            UpdateRing();
+        }
+
+        Size ComputeEllipseSize(double thickness, double actualWidth, double actualHeight)
+        {
+            double safeThickness = Math.Max(thickness, (double)0.0);
+            double width = Math.Max((actualWidth - safeThickness) / 2.0, 0.0);
+            double height = Math.Max((actualHeight - safeThickness) / 2.0, 0.0);
+
+            return new Size(width, height);
         }
 
         void ChangeVisualState()
@@ -96,7 +319,7 @@ namespace ModernWpf.Controls
             VisualStateManager.GoToState(this, TemplateSettings.MaxSideLength < 60 ? s_SmallStateName : s_LargeStateName, true);
         }
 
-        void ApplyTemplateSettings()
+        void UpdateSize()
         {
             // TemplateSetting properties from WUXC for backwards compatibility.
             var templateSettings = TemplateSettings;
@@ -135,6 +358,60 @@ namespace ModernWpf.Controls
 
             templateSettings.EllipseOffset = thicknessEllipseOffset;
             templateSettings.MaxSideLength = width;
+        }
+
+        void UpdateSegment()
+        {
+            var templateSettings = TemplateSettings;
+
+            double angle()
+            {
+                double normalizedRange()
+                {
+                    double minimum = Minimum;
+                    double range = Maximum - minimum;
+                    double delta = Value - minimum;
+
+                    double normalizedRange = (range == 0.0) ? 0.0 : (delta / range);
+                    // normalizedRange offsets calculation to display a full ring when value = 100%
+                    // std::nextafter is set as a float as winrt::Point takes floats
+                    return Math.Min(normalizedRange, 0.999999940395355224609375);
+                }
+
+                return 2 * Math.PI * normalizedRange();
+            }
+
+            double thickness = StrokeThickness;
+            var size = ComputeEllipseSize(thickness, ActualWidth, ActualHeight);
+            double translationFactor = Math.Max(thickness / 2.0, 0.0);
+
+            double x = (Math.Sin(angle()) * size.Width) + size.Width + translationFactor;
+            double y = (((Math.Cos(angle()) * size.Height) - size.Height) * -1) + translationFactor;
+
+            templateSettings.RingArcIsLargeArc = angle() >= Math.PI;
+            templateSettings.RingArcPoint = new Point(x, y);
+        }
+
+        void UpdateRing()
+        {
+            var templateSettings = TemplateSettings;
+
+            double thickness = StrokeThickness;
+            var size = ComputeEllipseSize(thickness, ActualWidth, ActualHeight);
+
+            double segmentWidth = size.Width;
+            double translationFactor = Math.Max(thickness / 2.0, 0.0);
+
+            templateSettings.OutlineFigureStartPoint = new Point(segmentWidth + translationFactor, translationFactor);
+
+            templateSettings.RingFigureStartPoint = new Point(segmentWidth + translationFactor, translationFactor);
+
+            templateSettings.OutlineArcSize = new Size(segmentWidth, size.Height);
+            templateSettings.OutlineArcPoint = new Point(segmentWidth + translationFactor - 0.05d, translationFactor);
+
+            templateSettings.RingArcSize = new Size(segmentWidth, size.Height);
+
+            UpdateSegment();
         }
     }
 }
