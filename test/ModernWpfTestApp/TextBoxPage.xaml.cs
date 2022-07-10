8f314517ea7adab450255c710c777e59821a3388
Yimeng Wu <me@kinnara.cn>
2022-07-10 23:04:37 +0800

    Port microsoft/microsoft-ui-xaml#3933

---
+++ test/ModernWpfTestApp/TextBoxPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace MUXControlsTestApp
+{
+    [TopLevelTestPage(Name = "TextBox")]
+    public sealed partial class TextBoxPage : TestPage
+    {
+        public TextBoxPage()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
