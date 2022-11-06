be5a9dc118da0d62a85f5cfb897c46998b25baf8
Yimeng Wu <me@kinnara.cn>
2022-11-06 19:35:56 +0800

    Port microsoft/microsoft-ui-xaml#3869

---
+++ ModernWpf/Controls/Primitives/BrushToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ModernWpf.Controls.Primitives
+{
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color)
+            {
+                return value;
+            }
+
+            if (value is SolidColorBrush brush)
+            {
+                return brush.Color;
+            }
+
+            return default(Color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
