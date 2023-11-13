d724375c8a1b1f6b94fa7da953f8acad3bd4c3be
Yimeng Wu <me@kinnara.cn>
2023-11-13 23:01:23 +0000

    Port microsoft/microsoft-ui-xaml#3393 (#611)

---
+++ test/ModernWpfTestApp/Samples/UniformGridLayoutDemo.xaml.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace MUXControlsTestApp.Samples
+{
+    public sealed partial class UniformGridLayoutDemo
+    {
+        public IEnumerable<int> collection;
+
+        public UniformGridLayoutDemo()
+        {
+            collection = Enumerable.Range(0, 40);
+            this.InitializeComponent();
+            UniformGridRepeater.ItemsSource = collection;
+        }
+
+        public void GetRepeaterActualHeightButtonClick(object sender, RoutedEventArgs e)
+        {
+            RepeaterActualHeightLabel.Text = UniformGridRepeater.ActualHeight.ToString();
+        }
+    }
+}
