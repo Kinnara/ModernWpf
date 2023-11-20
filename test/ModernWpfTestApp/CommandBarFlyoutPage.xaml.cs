e5c853836f1444ef0c30a969c56ab6073070909a
Yimeng Wu <me@kinnara.cn>
2023-11-20 23:32:42 +0000

    Port microsoft/microsoft-ui-xaml#4532 (#621)

--- test/ModernWpfTestApp/CommandBarFlyoutPage.xaml.cs
+++ test/ModernWpfTestApp/CommandBarFlyoutPage.xaml.cs
@@ -18,6 +18,8 @@ namespace MUXControlsTestApp
         CommandBarFlyout Flyout5 => GetResource<CommandBarFlyout>();
         CommandBarFlyout Flyout6 => GetResource<CommandBarFlyout>();
         CommandBarFlyout Flyout7 => GetResource<CommandBarFlyout>();
+        CommandBarFlyout Flyout8 => GetResource<CommandBarFlyout>();
+        CommandBarFlyout Flyout9 => GetResource<CommandBarFlyout>();
 
         public CommandBarFlyoutPage()
         {
@@ -146,6 +148,16 @@ namespace MUXControlsTestApp
         private void OnFlyoutTarget7Click(object sender, RoutedEventArgs e)
         {
             ShowFlyoutAt(Flyout7, FlyoutTarget7);
+        }
+
+        private void OnFlyoutTarget8Click(object sender, RoutedEventArgs e)
+        {
+            ShowFlyoutAt(Flyout8, FlyoutTarget8);
+        }
+
+        private void OnFlyoutTarget9Click(object sender, RoutedEventArgs e)
+        {
+            ShowFlyoutAt(Flyout9, FlyoutTarget9);
         }
 
         private void ShowFlyoutAt(FlyoutBase flyout, FrameworkElement targetElement, FlyoutShowMode showMode = FlyoutShowMode.Transient)
