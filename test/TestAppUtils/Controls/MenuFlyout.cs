9e76c4fb5aab59075a791d0c7130aa43344f9df4
Yimeng Wu <me@kinnara.cn>
2023-11-28 23:00:11 +0000

    Port microsoft/microsoft-ui-xaml#4587 (#630)

--- test/TestAppUtils/Controls/MenuFlyout.cs
+++ test/TestAppUtils/Controls/MenuFlyout.cs
@@ -1,4 +1,6 @@
-﻿namespace System.Windows.Controls
+﻿using ModernWpf.Controls;
+
+namespace System.Windows.Controls
 {
     public class MenuFlyout : ModernWpf.Controls.MenuFlyout { }
 
@@ -22,6 +24,55 @@
         private static void OnTextPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
         {
             ((MenuFlyoutItem)sender).OnTextPropertyChanged(args);
+        }
+
+        private void OnTextPropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            Header = (string)args.NewValue;
+        }
+
+        #endregion
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            this.InitializeStyle(DefaultStyleKey);
+        }
+    }
+
+    public class ToggleMenuFlyoutItem : MenuFlyoutItem
+    {
+        static ToggleMenuFlyoutItem()
+        {
+            IsCheckableProperty.OverrideMetadata(typeof(ToggleMenuFlyoutItem), new FrameworkPropertyMetadata(true, null, CoerceIsCheckable));
+        }
+
+        private static object CoerceIsCheckable(DependencyObject d, object baseValue)
+        {
+            return true;
+        }
+    }
+
+    public class RadioMenuFlyoutItem : RadioMenuItem
+    {
+        #region Text
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(RadioMenuFlyoutItem),
+                new PropertyMetadata(string.Empty, OnTextPropertyChanged));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        private static void OnTextPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((RadioMenuFlyoutItem)sender).OnTextPropertyChanged(args);
         }
 
         private void OnTextPropertyChanged(DependencyPropertyChangedEventArgs args)
