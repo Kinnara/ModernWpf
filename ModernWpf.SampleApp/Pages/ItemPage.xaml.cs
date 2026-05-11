cc6243cc2aa35d1a6813a4da95e46387ecf9e885
Yimeng Wu <me@kinnara.cn>
2026-05-11 12:41:45 +0100

    Port WinUI Gallery status info samples to WPF

--- ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
+++ ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
@@ -18,7 +18,8 @@ namespace ModernWpf.SampleApp.Pages
 
             _item = item ?? GalleryCatalog.Items.First();
             WpfSampleContent = FundamentalsSampleFactory.Create(_item.UniqueId)
-                ?? BasicInputSampleFactory.Create(_item.UniqueId);
+                ?? BasicInputSampleFactory.Create(_item.UniqueId)
+                ?? StatusInfoSampleFactory.Create(_item.UniqueId);
             SampleSnippets = LoadSampleSnippets(_item.UniqueId);
             RelatedItems = _item.RelatedControlIds
                 .Select(GalleryCatalog.FindItem)
