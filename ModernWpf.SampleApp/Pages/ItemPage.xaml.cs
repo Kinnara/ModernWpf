ef3cda24b31d553b77915ee87537b87b74bf41db
Yimeng Wu <me@kinnara.cn>
2026-05-11 13:09:47 +0100

    Port WinUI Gallery navigation samples to WPF

--- ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
+++ ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
@@ -26,7 +26,8 @@ namespace ModernWpf.SampleApp.Pages
                 ?? CollectionsSampleFactory.Create(_item.UniqueId)
                 ?? DateTimeSampleFactory.Create(_item.UniqueId)
                 ?? ScrollingSampleFactory.Create(_item.UniqueId)
-                ?? LayoutSampleFactory.Create(_item.UniqueId);
+                ?? LayoutSampleFactory.Create(_item.UniqueId)
+                ?? NavigationSampleFactory.Create(_item.UniqueId);
             SampleSnippets = LoadSampleSnippets(_item.UniqueId);
             RelatedItems = _item.RelatedControlIds
                 .Select(GalleryCatalog.FindItem)
