fe7edeb64698e2cdcc197ce5020f28fac31227f6
Yimeng Wu <me@kinnara.cn>
2026-05-11 12:44:12 +0100

    Port WinUI Gallery dialogs flyouts samples to WPF

--- ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
+++ ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
@@ -19,7 +19,8 @@ namespace ModernWpf.SampleApp.Pages
             _item = item ?? GalleryCatalog.Items.First();
             WpfSampleContent = FundamentalsSampleFactory.Create(_item.UniqueId)
                 ?? BasicInputSampleFactory.Create(_item.UniqueId)
-                ?? StatusInfoSampleFactory.Create(_item.UniqueId);
+                ?? StatusInfoSampleFactory.Create(_item.UniqueId)
+                ?? DialogsFlyoutsSampleFactory.Create(_item.UniqueId);
             SampleSnippets = LoadSampleSnippets(_item.UniqueId);
             RelatedItems = _item.RelatedControlIds
                 .Select(GalleryCatalog.FindItem)
