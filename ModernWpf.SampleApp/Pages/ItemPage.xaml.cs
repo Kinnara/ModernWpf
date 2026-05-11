1b17ae2b3decb317dbc3342ccb7478afcc303709
Yimeng Wu <me@kinnara.cn>
2026-05-11 12:58:31 +0100

    Port WinUI Gallery date time samples to WPF

--- ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
+++ ModernWpf.SampleApp/Pages/ItemPage.xaml.cs
@@ -23,7 +23,8 @@ namespace ModernWpf.SampleApp.Pages
                 ?? DialogsFlyoutsSampleFactory.Create(_item.UniqueId)
                 ?? DesignAccessibilitySampleFactory.Create(_item.UniqueId)
                 ?? MenusToolbarsSampleFactory.Create(_item.UniqueId)
-                ?? CollectionsSampleFactory.Create(_item.UniqueId);
+                ?? CollectionsSampleFactory.Create(_item.UniqueId)
+                ?? DateTimeSampleFactory.Create(_item.UniqueId);
             SampleSnippets = LoadSampleSnippets(_item.UniqueId);
             RelatedItems = _item.RelatedControlIds
                 .Select(GalleryCatalog.FindItem)
