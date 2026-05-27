ca577ac411d042d2ccb485c4f2209a2af6fa3897
Yimeng Wu <me@kinnara.cn>
2026-05-27 03:05:51 +0100

    Align ColorTile code-behind source shape

--- ModernWpf.Gallery/Controls/ColorTile.cs
+++ ModernWpf.Gallery/Controls/ColorTile.cs
@@ -7,68 +7,11 @@ using System.Windows.Input;
 
 namespace ModernWpf.Gallery.Controls
 {
-    public class ColorTile : UserControl
+    public partial class ColorTile : UserControl
     {
-        public static readonly DependencyProperty ColorBrushNameProperty =
-            DependencyProperty.Register(nameof(ColorBrushName), typeof(string), typeof(ColorTile), new PropertyMetadata(string.Empty));
-
-        public static readonly DependencyProperty ColorExplanationProperty =
-            DependencyProperty.Register(nameof(ColorExplanation), typeof(string), typeof(ColorTile), new PropertyMetadata(string.Empty));
-
-        public static readonly DependencyProperty ColorNameProperty =
-            DependencyProperty.Register(nameof(ColorName), typeof(string), typeof(ColorTile), new PropertyMetadata(string.Empty));
-
-        public static readonly DependencyProperty ColorValueProperty =
-            DependencyProperty.Register(nameof(ColorValue), typeof(string), typeof(ColorTile), new PropertyMetadata(string.Empty));
-
-        public static readonly DependencyProperty ShowSeparatorProperty =
-            DependencyProperty.Register(nameof(ShowSeparator), typeof(bool), typeof(ColorTile), new PropertyMetadata(true));
-
-        public static readonly DependencyProperty ShowWarningProperty =
-            DependencyProperty.Register(nameof(ShowWarning), typeof(bool), typeof(ColorTile), new PropertyMetadata(false));
-
-        public static readonly DependencyProperty TileRadiusProperty =
-            DependencyProperty.Register(nameof(TileRadius), typeof(CornerRadius), typeof(ColorTile), new PropertyMetadata(new CornerRadius(0)));
-
         static ColorTile()
         {
-            CommandManager.RegisterClassCommandBinding(typeof(ColorTile), new CommandBinding(ApplicationCommands.Copy, CopyColorBrushName));
-        }
-
-        public string ColorBrushName
-        {
-            get { return (string)GetValue(ColorBrushNameProperty); }
-            set { SetValue(ColorBrushNameProperty, value); }
-        }
-
-        public string ColorExplanation
-        {
-            get { return (string)GetValue(ColorExplanationProperty); }
-            set { SetValue(ColorExplanationProperty, value); }
-        }
-
-        public string ColorName
-        {
-            get { return (string)GetValue(ColorNameProperty); }
-            set { SetValue(ColorNameProperty, value); }
-        }
-
-        public string ColorValue
-        {
-            get { return (string)GetValue(ColorValueProperty); }
-            set { SetValue(ColorValueProperty, value); }
-        }
-
-        public bool ShowSeparator
-        {
-            get { return (bool)GetValue(ShowSeparatorProperty); }
-            set { SetValue(ShowSeparatorProperty, value); }
-        }
-
-        public bool ShowWarning
-        {
-            get { return (bool)GetValue(ShowWarningProperty); }
-            set { SetValue(ShowWarningProperty, value); }
+            CommandManager.RegisterClassCommandBinding(typeof(ColorTile), new CommandBinding(ApplicationCommands.Copy, Copy_ColorBrushName));
         }
 
         public CornerRadius TileRadius
@@ -77,10 +20,71 @@ namespace ModernWpf.Gallery.Controls
             set { SetValue(TileRadiusProperty, value); }
         }
 
-        private static void CopyColorBrushName(object sender, ExecutedRoutedEventArgs e)
+        public static readonly DependencyProperty TileRadiusProperty =
+            DependencyProperty.Register("TileRadius", typeof(CornerRadius), typeof(ColorTile), new PropertyMetadata(new CornerRadius(0)));
+
+        public string ColorName
         {
-            var colorTile = sender as ColorTile;
-            if (colorTile == null || string.IsNullOrEmpty(colorTile.ColorBrushName))
+            get { return (string)GetValue(ColorNameProperty); }
+            set { SetValue(ColorNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorNameProperty =
+            DependencyProperty.Register("ColorName", typeof(string), typeof(ColorTile), new PropertyMetadata(""));
+
+        public string ColorExplanation
+        {
+            get { return (string)GetValue(ColorExplanationProperty); }
+            set { SetValue(ColorExplanationProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorExplanationProperty =
+            DependencyProperty.Register("ColorExplanation", typeof(string), typeof(ColorTile), new PropertyMetadata(""));
+
+        public string ColorBrushName
+        {
+            get { return (string)GetValue(ColorBrushNameProperty); }
+            set { SetValue(ColorBrushNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorBrushNameProperty =
+            DependencyProperty.Register("ColorBrushName", typeof(string), typeof(ColorTile), new PropertyMetadata(""));
+
+        public string ColorValue
+        {
+            get { return (string)GetValue(ColorValueProperty); }
+            set { SetValue(ColorValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorValueProperty =
+            DependencyProperty.Register("ColorValue", typeof(string), typeof(ColorTile), new PropertyMetadata(""));
+
+        public bool ShowSeparator
+        {
+            get { return (bool)GetValue(ShowSeparatorProperty); }
+            set { SetValue(ShowSeparatorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowSeparatorProperty =
+            DependencyProperty.Register("ShowSeparator", typeof(bool), typeof(ColorTile), new PropertyMetadata(true));
+
+        public bool ShowWarning
+        {
+            get { return (bool)GetValue(ShowWarningProperty); }
+            set { SetValue(ShowWarningProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowWarningProperty =
+            DependencyProperty.Register("ShowWarning", typeof(bool), typeof(ColorTile), new PropertyMetadata(false));
+
+        private static void Copy_ColorBrushName(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is ColorTile colorTile))
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(colorTile.ColorBrushName))
             {
                 return;
             }
--- test/ModernWpf.Gallery.Tests/WpfGallerySourceShapeTests.cs
+++ test/ModernWpf.Gallery.Tests/WpfGallerySourceShapeTests.cs
@@ -722,6 +722,46 @@ namespace ModernWpf.Gallery.Tests
         }
 
         [TestMethod]
+        public void SharedColorTileCodeBehindKeepsOfficialMemberAndCopyHandlerSourceShape()
+        {
+            var source = ReadRepoFile(
+                "ModernWpf.Gallery",
+                "Controls",
+                "ColorTile.cs");
+
+            AssertContainsInOrder(
+                source,
+                "public partial class ColorTile : UserControl",
+                "static ColorTile()",
+                "CommandManager.RegisterClassCommandBinding(typeof(ColorTile), new CommandBinding(ApplicationCommands.Copy, Copy_ColorBrushName));",
+                "public CornerRadius TileRadius",
+                "public static readonly DependencyProperty TileRadiusProperty",
+                "DependencyProperty.Register(\"TileRadius\", typeof(CornerRadius), typeof(ColorTile), new PropertyMetadata(new CornerRadius(0)));",
+                "public string ColorName",
+                "public static readonly DependencyProperty ColorNameProperty",
+                "DependencyProperty.Register(\"ColorName\", typeof(string), typeof(ColorTile), new PropertyMetadata(\"\"));",
+                "public string ColorExplanation",
+                "public static readonly DependencyProperty ColorExplanationProperty",
+                "DependencyProperty.Register(\"ColorExplanation\", typeof(string), typeof(ColorTile), new PropertyMetadata(\"\"));",
+                "public string ColorBrushName",
+                "public static readonly DependencyProperty ColorBrushNameProperty",
+                "DependencyProperty.Register(\"ColorBrushName\", typeof(string), typeof(ColorTile), new PropertyMetadata(\"\"));",
+                "public string ColorValue",
+                "public static readonly DependencyProperty ColorValueProperty",
+                "DependencyProperty.Register(\"ColorValue\", typeof(string), typeof(ColorTile), new PropertyMetadata(\"\"));",
+                "public bool ShowSeparator",
+                "public static readonly DependencyProperty ShowSeparatorProperty",
+                "DependencyProperty.Register(\"ShowSeparator\", typeof(bool), typeof(ColorTile), new PropertyMetadata(true));",
+                "public bool ShowWarning",
+                "public static readonly DependencyProperty ShowWarningProperty",
+                "DependencyProperty.Register(\"ShowWarning\", typeof(bool), typeof(ColorTile), new PropertyMetadata(false));",
+                "private static void Copy_ColorBrushName(object sender, RoutedEventArgs e)",
+                "if (!(sender is ColorTile colorTile))",
+                "Clipboard.SetText(colorTile.ColorBrushName);",
+                "RaiseNotification(colorTile);");
+        }
+
+        [TestMethod]
         public void HomePageKeepsOfficialDashboardCardListDeclarationSourceShape()
         {
             var xaml = ReadRepoFile(
