0b4113fb944e2b736ec0f68c744fbf14fc629de9
Yimeng Wu <me@kinnara.cn>
2026-05-28 19:56:14 +0100

    Cover AutoSuggestBox style resources

--- test/ModernWpf.WinUI.Tests/AutoSuggestBox/AutoSuggestBoxApiTests.cs
+++ test/ModernWpf.WinUI.Tests/AutoSuggestBox/AutoSuggestBoxApiTests.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModernWpf;
 using ModernWpf.Controls;
@@ -30,19 +31,97 @@ public class AutoSuggestBoxApiTests
         {
             TestApplication.EnsureInitialized();
 
-            var autoSuggestBox = new MuxAutoSuggestBox();
+            var controlsResources = new ResourceDictionary
+            {
+                Source = new Uri("/ModernWpf.Controls;component/AutoSuggestBox/AutoSuggestBox.xaml", UriKind.Relative)
+            };
+            var sharedResources = new ResourceDictionary
+            {
+                Source = new Uri("/ModernWpf;component/Styles/AutoSuggestBox.xaml", UriKind.Relative)
+            };
+
+            AssertResource(sharedResources, "AutoSuggestBoxTopHeaderMargin", new Thickness(0, 0, 0, 8));
+            AssertResource(sharedResources, "AutoSuggestBoxInnerButtonMargin", new Thickness(1, 3, 1, 3));
+            AssertResource(sharedResources, "AutoSuggestBoxDeleteButtonMargin", new Thickness(0, 4, 0, 4));
+            AssertResource(sharedResources, "AutoSuggestBoxQueryButtonPadding", new Thickness(3, 2, 3, 2));
+            AssertResource(sharedResources, "AutoSuggestBoxLeftButtonMargin", 3d);
+            AssertResource(sharedResources, "AutoSuggestBoxRightButtonMargin", 4d);
+
+            var textBoxStyle = (Style)sharedResources["AutoSuggestBoxTextBoxStyle"];
+            AssertSetterValue(textBoxStyle, FrameworkElement.OverridesDefaultStyleProperty, true);
+            AssertDynamicResourceSetter(textBoxStyle, FrameworkElement.MinWidthProperty, "TextControlThemeMinWidth");
+            AssertDynamicResourceSetter(textBoxStyle, FrameworkElement.MinHeightProperty, "TextControlThemeMinHeight");
+            AssertDynamicResourceSetter(textBoxStyle, Control.ForegroundProperty, "TextControlForeground");
+            AssertDynamicResourceSetter(textBoxStyle, Control.BackgroundProperty, "TextControlBackground");
+            AssertDynamicResourceSetter(textBoxStyle, Control.BorderBrushProperty, "TextControlBorderBrush");
+            AssertDynamicResourceSetter(textBoxStyle, TextBoxBase.SelectionBrushProperty, "TextControlSelectionHighlightColor");
+            AssertDynamicResourceSetter(textBoxStyle, Control.BorderThicknessProperty, "TextControlBorderThemeThickness");
+            AssertDynamicResourceSetter(textBoxStyle, Control.FontFamilyProperty, "ContentControlThemeFontFamily");
+            AssertDynamicResourceSetter(textBoxStyle, Control.FontSizeProperty, "ControlContentThemeFontSize");
+            AssertSetterValue(textBoxStyle, ScrollViewer.HorizontalScrollBarVisibilityProperty, ScrollBarVisibility.Hidden);
+            AssertSetterValue(textBoxStyle, ScrollViewer.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Hidden);
+            AssertSetterValue(textBoxStyle, ScrollViewer.IsDeferredScrollingEnabledProperty, false);
+            AssertDynamicResourceSetter(textBoxStyle, Control.PaddingProperty, "TextControlThemePadding");
+            AssertSetterValue(textBoxStyle, KeyboardNavigation.TabNavigationProperty, KeyboardNavigationMode.None);
+            AssertSetterValue(textBoxStyle, Control.FocusVisualStyleProperty, null);
+            AssertSetterValue(textBoxStyle, UIElement.AllowDropProperty, true);
+            AssertDynamicResourceSetter(textBoxStyle, Control.ContextMenuProperty, "TextControlContextMenu");
+            AssertDynamicResourceSetter(textBoxStyle, Validation.ErrorTemplateProperty, "TextControlValidationErrorTemplate");
+            Assert.IsInstanceOfType(FindSetter(textBoxStyle, Control.TemplateProperty)?.Value, typeof(ControlTemplate));
+
+            var style = (Style)controlsResources[typeof(MuxAutoSuggestBox)];
+            AssertSetterValue(style, FrameworkElement.VerticalAlignmentProperty, VerticalAlignment.Stretch);
+            AssertSetterValue(style, Control.VerticalContentAlignmentProperty, VerticalAlignment.Stretch);
+            AssertSetterValue(style, Control.HorizontalContentAlignmentProperty, HorizontalAlignment.Stretch);
+            AssertSetterValue(style, Control.IsTabStopProperty, false);
+            AssertDynamicResourceSetter(style, Control.ForegroundProperty, "TextControlForeground");
+            AssertDynamicResourceSetter(style, Control.BackgroundProperty, "TextControlBackground");
+            AssertDynamicResourceSetter(style, Control.BorderBrushProperty, "TextControlBorderBrush");
+            AssertDynamicResourceSetter(style, Control.BorderThicknessProperty, "TextControlBorderThemeThickness");
+            AssertDynamicResourceSetter(style, Control.FontFamilyProperty, "ContentControlThemeFontFamily");
+            AssertDynamicResourceSetter(style, Control.FontSizeProperty, "ControlContentThemeFontSize");
+            AssertDynamicResourceSetter(style, MuxAutoSuggestBox.TextBoxStyleProperty, "AutoSuggestBoxTextBoxStyle");
+            AssertDynamicResourceSetter(style, MuxAutoSuggestBox.UseSystemFocusVisualsProperty, "IsApplicationFocusVisualKindReveal");
+            AssertDynamicResourceSetter(style, Control.FocusVisualStyleProperty, SystemParameters.FocusVisualStyleKey);
+            AssertDynamicResourceSetter(style, MuxAutoSuggestBox.CornerRadiusProperty, "ControlCornerRadius");
+            AssertDynamicResourceSetter(style, ItemsControl.ItemContainerStyleProperty, typeof(System.Windows.Controls.ListViewItem));
+            Assert.IsInstanceOfType(FindSetter(style, Control.TemplateProperty)?.Value, typeof(ControlTemplate));
+
+            var suggestions = new List<string> { "alpha", "beta" };
+            var autoSuggestBox = new MuxAutoSuggestBox
+            {
+                ItemsSource = suggestions,
+                Style = style,
+                Width = 400
+            };
+            autoSuggestBox.Resources.MergedDictionaries.Add(controlsResources);
+            autoSuggestBox.Resources.MergedDictionaries.Add(sharedResources);
 
             using var host = new TestWindowHost(autoSuggestBox, width: 400, height: 120);
             host.UpdateLayout();
 
+            Assert.AreSame(style, autoSuggestBox.Style);
             Assert.AreEqual(VerticalAlignment.Stretch, autoSuggestBox.VerticalAlignment);
             Assert.AreEqual(VerticalAlignment.Stretch, autoSuggestBox.VerticalContentAlignment);
             Assert.AreEqual(HorizontalAlignment.Stretch, autoSuggestBox.HorizontalContentAlignment);
+            Assert.IsFalse(autoSuggestBox.IsTabStop);
+            AssertBrushEquals((Brush)autoSuggestBox.TryFindResource("TextControlForeground"), autoSuggestBox.Foreground);
+            AssertBrushEquals((Brush)autoSuggestBox.TryFindResource("TextControlBackground"), autoSuggestBox.Background);
+            AssertBrushEquals((Brush)autoSuggestBox.TryFindResource("TextControlBorderBrush"), autoSuggestBox.BorderBrush);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("TextControlBorderThemeThickness"), autoSuggestBox.BorderThickness);
+            Assert.AreEqual(
+                ((FontFamily)autoSuggestBox.TryFindResource("ContentControlThemeFontFamily")).Source,
+                autoSuggestBox.FontFamily.Source);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("ControlContentThemeFontSize"), autoSuggestBox.FontSize);
             Assert.IsNotNull(autoSuggestBox.TextBoxStyle);
             Assert.IsTrue(autoSuggestBox.AutoMaximizeSuggestionArea);
             Assert.AreEqual(LightDismissOverlayMode.Auto, autoSuggestBox.LightDismissOverlayMode);
             Assert.AreEqual(ControlHeaderPlacement.Top, autoSuggestBox.HeaderPlacement);
             Assert.AreSame(autoSuggestBox.TryFindResource("AutoSuggestBoxTextBoxStyle"), autoSuggestBox.TextBoxStyle);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("IsApplicationFocusVisualKindReveal"), autoSuggestBox.UseSystemFocusVisuals);
+            Assert.AreSame(autoSuggestBox.TryFindResource(SystemParameters.FocusVisualStyleKey), autoSuggestBox.FocusVisualStyle);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("ControlCornerRadius"), autoSuggestBox.CornerRadius);
+            Assert.AreSame(autoSuggestBox.TryFindResource(typeof(System.Windows.Controls.ListViewItem)), autoSuggestBox.ItemContainerStyle);
             AssertGlobalResourceValue(autoSuggestBox, "AutoSuggestBoxTopHeaderMargin", new Thickness(0, 0, 0, 8));
             AssertGlobalResourceValue(autoSuggestBox, "AutoSuggestBoxInnerButtonMargin", new Thickness(1, 3, 1, 3));
             AssertGlobalResourceValue(autoSuggestBox, "AutoSuggestBoxDeleteButtonMargin", new Thickness(0, 4, 0, 4));
@@ -52,6 +131,83 @@ public class AutoSuggestBoxApiTests
 
             var textBox = FindTemplateChild<TextBox>(autoSuggestBox, "TextBox");
             Assert.AreSame(autoSuggestBox.TextBoxStyle, textBox.Style);
+            Assert.AreEqual(textBox.TryFindResource("TextControlThemeMinWidth"), textBox.MinWidth);
+            Assert.AreEqual(textBox.TryFindResource("TextControlThemeMinHeight"), textBox.MinHeight);
+            AssertBrushEquals(autoSuggestBox.Foreground, textBox.Foreground);
+            AssertBrushEquals(autoSuggestBox.Background, textBox.Background);
+            AssertBrushEquals(autoSuggestBox.BorderBrush, textBox.BorderBrush);
+            AssertBrushEquals((Brush)textBox.TryFindResource("TextControlSelectionHighlightColor"), textBox.SelectionBrush);
+            Assert.AreEqual(autoSuggestBox.BorderThickness, textBox.BorderThickness);
+            Assert.AreEqual(autoSuggestBox.FontSize, textBox.FontSize);
+            Assert.AreEqual(autoSuggestBox.FontFamily.Source, textBox.FontFamily.Source);
+            Assert.AreEqual(autoSuggestBox.VerticalContentAlignment, textBox.VerticalContentAlignment);
+            Assert.AreEqual(autoSuggestBox.HorizontalContentAlignment, textBox.HorizontalContentAlignment);
+            Assert.AreEqual(new Thickness(0), textBox.Margin);
+            Assert.AreEqual(textBox.TryFindResource("TextControlThemePadding"), textBox.Padding);
+            Assert.AreSame(textBox.TryFindResource("TextControlContextMenu"), textBox.ContextMenu);
+            Assert.AreSame(textBox.TryFindResource("TextControlValidationErrorTemplate"), Validation.GetErrorTemplate(textBox));
+
+            var deleteButton = FindTemplateChild<Button>(textBox, "DeleteButton");
+            deleteButton.ApplyTemplate();
+            var deleteButtonChrome = FindNamedDescendant<Border>(deleteButton, "ButtonLayoutGrid");
+            var deleteButtonGlyph = FindNamedDescendant<FontIconFallback>(deleteButton, "GlyphElement");
+            Assert.AreEqual(32d, deleteButton.Width);
+            Assert.AreEqual(VerticalAlignment.Stretch, deleteButton.VerticalAlignment);
+            Assert.AreEqual(textBox.BorderThickness, deleteButton.BorderThickness);
+            Assert.AreEqual(textBox.TryFindResource("HelperButtonThemePadding"), deleteButton.Padding);
+            Assert.IsFalse(deleteButton.Focusable);
+            AssertBrushEquals((Brush)deleteButton.TryFindResource("TextControlButtonBackground"), deleteButtonChrome.Background);
+            AssertBrushEquals((Brush)deleteButton.TryFindResource("TextControlButtonBorderBrush"), deleteButtonChrome.BorderBrush);
+            Assert.AreEqual(deleteButton.BorderThickness, deleteButtonChrome.BorderThickness);
+            Assert.AreEqual(deleteButton.TryFindResource("AutoSuggestBoxDeleteButtonMargin"), deleteButtonChrome.Margin);
+            Assert.AreEqual(deleteButton.TryFindResource("AutoSuggestBoxIconFontSize"), deleteButtonGlyph.FontSize);
+            Assert.AreEqual(((FontFamily)deleteButton.TryFindResource("SymbolThemeFontFamily")).Source, deleteButtonGlyph.FontFamily.Source);
+
+            var queryButton = FindTemplateChild<Button>(textBox, "QueryButton");
+            queryButton.ApplyTemplate();
+            var queryButtonPresenter = FindNamedDescendant<ContentPresenterEx>(queryButton, "ContentPresenter");
+            Assert.AreEqual(32d, queryButton.Width);
+            Assert.AreEqual(28d, queryButton.Height);
+            Assert.AreEqual(new Thickness(2, 0, 0, 0), queryButton.Margin);
+            Assert.AreEqual(VerticalAlignment.Stretch, queryButton.VerticalAlignment);
+            Assert.AreEqual(textBox.BorderThickness, queryButton.BorderThickness);
+            Assert.AreEqual(textBox.TryFindResource("HelperButtonThemePadding"), queryButton.Padding);
+            Assert.IsFalse(queryButton.Focusable);
+            Assert.IsTrue(ButtonHelper.GetVisualStateSettersEnabled(queryButton));
+            AssertBrushEquals((Brush)queryButton.TryFindResource("TextControlButtonBackground"), queryButtonPresenter.Background);
+            AssertBrushEquals((Brush)queryButton.TryFindResource("TextControlButtonBorderBrush"), queryButtonPresenter.BorderBrush);
+            Assert.AreEqual(queryButton.BorderThickness, queryButtonPresenter.BorderThickness);
+            Assert.AreEqual(queryButton.TryFindResource("AutoSuggestBoxInnerButtonMargin"), queryButtonPresenter.Margin);
+            Assert.AreEqual(queryButton.TryFindResource("AutoSuggestBoxIconFontSize"), queryButtonPresenter.FontSize);
+            Assert.AreEqual(queryButton.Padding, queryButtonPresenter.Padding);
+
+            var popup = FindTemplateChild<Popup>(autoSuggestBox, "SuggestionsPopup");
+            Assert.IsTrue(popup.AllowsTransparency);
+            Assert.AreEqual(PlacementMode.Bottom, popup.Placement);
+
+            var shadowChrome = popup.Child as ThemeShadowChrome
+                ?? throw new AssertFailedException("Expected AutoSuggestBox suggestions popup child to be ThemeShadowChrome.");
+            Assert.AreEqual(ThemeShadowChromeWindowedPopupInsetMode.Medium, shadowChrome.WindowedPopupInsetMode);
+            Assert.AreEqual(autoSuggestBox.MaxSuggestionListHeight, shadowChrome.MaxHeight);
+
+            var suggestionsContainer = FindTemplateChild<Border>(autoSuggestBox, "SuggestionsContainer");
+            Assert.AreSame(suggestionsContainer, shadowChrome.Child);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("AutoSuggestListMargin"), suggestionsContainer.Padding);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("AutoSuggestListBorderThemeThickness"), suggestionsContainer.BorderThickness);
+            AssertBrushEquals((Brush)autoSuggestBox.TryFindResource("AutoSuggestBoxSuggestionsListBorderBrush"), suggestionsContainer.BorderBrush);
+            AssertBrushEquals((Brush)autoSuggestBox.TryFindResource("AutoSuggestBoxSuggestionsListBackground"), suggestionsContainer.Background);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("OverlayCornerRadius"), suggestionsContainer.CornerRadius);
+
+            var suggestionsList = FindTemplateChild<AutoSuggestBoxListView>(autoSuggestBox, "SuggestionsList");
+            Assert.AreSame(autoSuggestBox.TryFindResource(typeof(System.Windows.Controls.ListView)), suggestionsList.Style);
+            Assert.AreEqual(autoSuggestBox.DisplayMemberPath, suggestionsList.DisplayMemberPath);
+            Assert.AreEqual(autoSuggestBox.TextMemberPath, suggestionsList.SelectedValuePath);
+            Assert.IsTrue(suggestionsList.IsItemClickEnabled);
+            Assert.AreSame(autoSuggestBox.ItemContainerStyle, suggestionsList.ItemContainerStyle);
+            Assert.AreSame(suggestions, suggestionsList.ItemsSource);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("AutoSuggestListMaxHeight"), suggestionsList.MaxHeight);
+            Assert.AreEqual(autoSuggestBox.TryFindResource("AutoSuggestListPadding"), suggestionsList.Margin);
+            Assert.AreEqual(ScrollBarVisibility.Disabled, ScrollViewer.GetHorizontalScrollBarVisibility(suggestionsList));
 
             foreach (var themeName in new[] { "Light", "Dark", "HighContrast" })
             {
@@ -443,6 +599,46 @@ public class AutoSuggestBoxApiTests
         Assert.AreSame(themeDictionary[expectedResourceKey], themeDictionary[resourceKey], $"{themeName}:{resourceKey}");
     }
 
+    private static void AssertResource(ResourceDictionary resources, string key, object expectedValue)
+    {
+        Assert.IsTrue(resources.Contains(key), $"Expected resource '{key}' to exist.");
+        Assert.AreEqual(expectedValue, resources[key], $"Unexpected value for '{key}'.");
+    }
+
+    private static void AssertSetterValue(Style style, DependencyProperty property, object? expectedValue)
+    {
+        var setter = FindSetter(style, property);
+        Assert.IsNotNull(setter, $"Expected local setter for {property.Name}.");
+        Assert.AreEqual(expectedValue, setter!.Value);
+    }
+
+    private static void AssertDynamicResourceSetter(Style style, DependencyProperty property, object expectedResourceKey)
+    {
+        var setter = FindSetter(style, property);
+        Assert.IsNotNull(setter, $"Expected local setter for {property.Name}.");
+
+        var dynamicResource = setter!.Value as DynamicResourceExtension;
+        Assert.IsNotNull(dynamicResource, $"Expected {property.Name} to use a dynamic resource.");
+        Assert.AreEqual(expectedResourceKey, dynamicResource!.ResourceKey);
+    }
+
+    private static Setter? FindSetter(Style style, DependencyProperty property)
+    {
+        for (var current = style; current != null; current = current.BasedOn)
+        {
+            var setter = current.Setters
+                .OfType<Setter>()
+                .SingleOrDefault(item => item.Property == property);
+
+            if (setter != null)
+            {
+                return setter;
+            }
+        }
+
+        return null;
+    }
+
     private static void AssertGlobalResourceValue<T>(FrameworkElement element, string resourceKey, T expectedValue)
     {
         var resource = element.TryFindResource(resourceKey);
@@ -462,6 +658,35 @@ public class AutoSuggestBoxApiTests
         return element.TryFindResource("OverlayCornerRadius") is CornerRadius radius
             ? radius
             : default;
+    }
+
+    private static void AssertBrushEquals(Brush expected, Brush actual)
+    {
+        Assert.IsNotNull(expected);
+        Assert.IsNotNull(actual);
+
+        if (expected is SolidColorBrush expectedSolid && actual is SolidColorBrush actualSolid)
+        {
+            Assert.AreEqual(expectedSolid.Color, actualSolid.Color);
+            Assert.AreEqual(expectedSolid.Opacity, actualSolid.Opacity);
+            return;
+        }
+
+        Assert.AreEqual(expected.ToString(), actual.ToString());
+    }
+
+    private static T FindNamedDescendant<T>(DependencyObject root, string name)
+        where T : FrameworkElement
+    {
+        foreach (var descendant in VisualTreeTestHelper.EnumerateDescendants(root))
+        {
+            if (descendant is T element && element.Name == name)
+            {
+                return element;
+            }
+        }
+
+        throw new InvalidOperationException($"Could not find descendant named '{name}'.");
     }
 
     private static void AssertCornerRadiusMatchesOpenDirection(
