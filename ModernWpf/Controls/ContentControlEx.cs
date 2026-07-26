1fcbc81f957e893495bd1e9188eed690d78723fc
Yimeng Wu <me@kinnara.cn>
2026-07-26 16:40:25 +0100

    Run ContentControlEx content transitions (#734)
    Fixes #127.

--- ModernWpf/Controls/ContentControlEx.cs
+++ ModernWpf/Controls/ContentControlEx.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 using ModernWpf.Controls.Primitives;
 using ModernWpf.Media.Animation;
 
 namespace ModernWpf.Controls
 {
+    [TemplatePart(Name = ContentPresenterTemplatePartName, Type = typeof(ContentPresenterEx))]
+    [TemplatePart(Name = PreviousContentPresenterTemplatePartName, Type = typeof(ContentPresenterEx))]
     public class ContentControlEx : ContentControl
     {
         static ContentControlEx()
@@ -13,6 +18,9 @@ namespace ModernWpf.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentControlEx), new FrameworkPropertyMetadata(typeof(ContentControlEx)));
             HorizontalContentAlignmentProperty.OverrideMetadata(typeof(ContentControlEx), new FrameworkPropertyMetadata(HorizontalAlignment.Left));
             VerticalContentAlignmentProperty.OverrideMetadata(typeof(ContentControlEx), new FrameworkPropertyMetadata(VerticalAlignment.Top));
+            ContentProperty.OverrideMetadata(
+                typeof(ContentControlEx),
+                new FrameworkPropertyMetadata(OnContentPropertyChanged));
         }
 
         #region BackgroundSizing
@@ -75,7 +83,7 @@ namespace ModernWpf.Controls
                 nameof(ContentTransitions),
                 typeof(TransitionCollection),
                 typeof(ContentControlEx),
-                new PropertyMetadata(null));
+                new PropertyMetadata(null, OnContentTransitionsPropertyChanged));
 
         public TransitionCollection ContentTransitions
         {
@@ -138,9 +146,68 @@ namespace ModernWpf.Controls
 
         public override void OnApplyTemplate()
         {
+            StopTransition();
+
             base.OnApplyTemplate();
 
             _contentPresenter = GetTemplateChild(ContentPresenterTemplatePartName) as ContentPresenter;
+            _previousContentPresenter = GetTemplateChild(PreviousContentPresenterTemplatePartName) as ContentPresenter;
+
+            if (_previousContentPresenter != null)
+            {
+                _previousContentPresenter.Content = null;
+                _previousContentPresenter.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private static void OnContentPropertyChanged(
+            DependencyObject sender,
+            DependencyPropertyChangedEventArgs args)
+        {
+            ((ContentControlEx)sender).StartContentTransition(args.OldValue, args.NewValue);
+        }
+
+        private void StartContentTransition(object oldContent, object newContent)
+        {
+            StopTransition();
+
+            if (_contentPresenter == null ||
+                _previousContentPresenter == null ||
+                ReferenceEquals(oldContent, newContent) ||
+                !Helper.IsAnimationsEnabled)
+            {
+                return;
+            }
+
+            var transitionInfo = GetNavigationTransitionInfo();
+            if (transitionInfo == null)
+            {
+                return;
+            }
+
+            _exitAnimation = oldContent == null
+                ? null
+                : transitionInfo.GetExitAnimation(_previousContentPresenter, false);
+            _enterAnimation = newContent == null
+                ? null
+                : transitionInfo.GetEnterAnimation(_contentPresenter, false);
+
+            if (_exitAnimation == null && _enterAnimation == null)
+            {
+                return;
+            }
+
+            _previousContentPresenter.Content = oldContent;
+            _previousContentPresenter.Opacity = 1;
+            _previousContentPresenter.Visibility =
+                oldContent == null ? Visibility.Collapsed : Visibility.Visible;
+            _previousContentPresenter.IsHitTestVisible = false;
+
+            _contentPresenter.Opacity = 0;
+            _contentPresenter.Visibility = Visibility.Visible;
+            _contentPresenter.IsHitTestVisible = false;
+
+            BeginTransition();
         }
 
         private UIElement GetContentTemplateRoot()
@@ -162,7 +229,152 @@ namespace ModernWpf.Controls
             return null;
         }
 
+        private static void OnContentTransitionsPropertyChanged(
+            DependencyObject sender,
+            DependencyPropertyChangedEventArgs args)
+        {
+            ((ContentControlEx)sender).StopTransition();
+        }
+
+        private NavigationTransitionInfo GetNavigationTransitionInfo()
+        {
+            var transition = ContentTransitions?
+                .OfType<NavigationThemeTransition>()
+                .LastOrDefault();
+
+            if (transition == null)
+            {
+                return null;
+            }
+
+            return transition.DefaultNavigationTransitionInfo ??
+                   new EntranceNavigationTransitionInfo();
+        }
+
+        private void BeginTransition()
+        {
+            if (_exitAnimation != null)
+            {
+                _exitAnimation.Completed += OnExitAnimationCompleted;
+            }
+
+            if (_enterAnimation != null)
+            {
+                _enterAnimation.Completed += OnEnterAnimationCompleted;
+            }
+
+            _asyncBeginTransition = Dispatcher.BeginInvoke(() =>
+            {
+                _asyncBeginTransition = null;
+
+                if (_exitAnimation != null)
+                {
+                    _exitAnimation.Begin();
+                }
+                else
+                {
+                    BeginEnterAnimation();
+                }
+            }, DispatcherPriority.ApplicationIdle);
+        }
+
+        private void BeginEnterAnimation()
+        {
+            ClearPreviousContent();
+
+            if (_contentPresenter != null)
+            {
+                _contentPresenter.Opacity = 1;
+            }
+
+            if (_enterAnimation != null)
+            {
+                _enterAnimation.Begin();
+            }
+            else
+            {
+                StopTransition();
+            }
+        }
+
+        private void OnExitAnimationCompleted(object sender, EventArgs e)
+        {
+            StopExitAnimation();
+
+            if (_enterAnimation != null)
+            {
+                BeginEnterAnimation();
+            }
+            else
+            {
+                StopTransition();
+            }
+        }
+
+        private void OnEnterAnimationCompleted(object sender, EventArgs e)
+        {
+            StopEnterAnimation();
+            StopTransition();
+        }
+
+        private void StopTransition()
+        {
+            if (_asyncBeginTransition != null)
+            {
+                _asyncBeginTransition.Abort();
+                _asyncBeginTransition = null;
+            }
+
+            StopExitAnimation();
+            StopEnterAnimation();
+            ClearPreviousContent();
+
+            if (_contentPresenter != null)
+            {
+                _contentPresenter.Visibility = Visibility.Visible;
+                _contentPresenter.ClearValue(OpacityProperty);
+                _contentPresenter.ClearValue(IsHitTestVisibleProperty);
+            }
+        }
+
+        private void StopExitAnimation()
+        {
+            if (_exitAnimation != null)
+            {
+                _exitAnimation.Completed -= OnExitAnimationCompleted;
+                _exitAnimation.Stop();
+                _exitAnimation = null;
+            }
+        }
+
+        private void StopEnterAnimation()
+        {
+            if (_enterAnimation != null)
+            {
+                _enterAnimation.Completed -= OnEnterAnimationCompleted;
+                _enterAnimation.Stop();
+                _enterAnimation = null;
+            }
+        }
+
+        private void ClearPreviousContent()
+        {
+            if (_previousContentPresenter != null)
+            {
+                _previousContentPresenter.Content = null;
+                _previousContentPresenter.Visibility = Visibility.Collapsed;
+                _previousContentPresenter.ClearValue(OpacityProperty);
+                _previousContentPresenter.ClearValue(IsHitTestVisibleProperty);
+            }
+        }
+
         private const string ContentPresenterTemplatePartName = "PART_ContentPresenter";
+        private const string PreviousContentPresenterTemplatePartName = "PART_PreviousContentPresenter";
+
         private ContentPresenter _contentPresenter;
+        private ContentPresenter _previousContentPresenter;
+        private NavigationAnimation _exitAnimation;
+        private NavigationAnimation _enterAnimation;
+        private DispatcherOperation _asyncBeginTransition;
     }
 }
