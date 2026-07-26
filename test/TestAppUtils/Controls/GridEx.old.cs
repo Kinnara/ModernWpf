ccd0df7b90b767c3dcb18f791cd4281c570ff6e9
Yimeng Wu <me@kinnara.cn>
2026-07-26 22:50:51 +0100

    Remove obsolete repository assets (#740)
    Remove verified-unreferenced documentation screenshots, Gallery media, legacy WinMD files, obsolete test helpers, and stale project entries. Add a Gallery asset-manifest regression test to protect retained resources.

--- test/TestAppUtils/Controls/GridEx.old.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Windows.Controls
-{
-    public class GridEx : PanelEx<Grid>
-    {
-        public ColumnDefinitionCollection ColumnDefinitions => ItemsHost.ColumnDefinitions;
-        public RowDefinitionCollection RowDefinitions => ItemsHost.RowDefinitions;
-    }
-}
